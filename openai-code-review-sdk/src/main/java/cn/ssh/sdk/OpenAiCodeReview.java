876dc88d63f3419ae953fc8b89660e49d03efd91
vadymsun <vadymsun@163.com>
2024-08-12 12:47:15 +0800

    init project

---
+++ openai-code-review-sdk/src/main/java/cn/ssh/sdk/OpenAiCodeReview.java
@@ -0,0 +1,7 @@
+package cn.ssh.sdk;
+
+public class OpenAiCodeReview {
+    public static void main(String[] args) {
+        System.out.println("测试执行");
+    }
+}
---
+++ opneai-code-review-test/src/main/java/cn/ssh/Application.java
@@ -0,0 +1,4 @@
+package cn.ssh;
+
+public class Application {
+}
